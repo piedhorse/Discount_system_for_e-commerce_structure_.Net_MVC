9483099c3d0493d9664a14866d7898353b7f93fd
piedhorse <umutalacatli@icloud.com>
2023-03-14 02:15:06 +0300

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models.Entity;
+
+namespace WebApplication2.Controllers
+{
+    public class AdminController : Controller
+    {
+        FGDBEntities1 FGDB = new FGDBEntities1();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var context = FGDB.DiscountTBL.ToList();
+            return View(context);
+        }
+
+        public ActionResult RemoveCampaign(int id)
+        {
+            var campaign = FGDB.DiscountTBL.Find(id);
+            FGDB.DiscountTBL.Remove(campaign);
+            FGDB.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+        public ActionResult CampaignUpdate(DiscountTBL discount)
+        {
+            var urun = FGDB.DiscountTBL.Find(discount.ID);
+            urun.startdate = discount.startdate;
+            urun.enddate = discount.enddate;
+            urun.discountedproducts = discount.discountedproducts;
+            urun.discountamount = discount.discountamount;
+            urun.campaignname = discount.campaignname;
+            FGDB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CampaignUpdateinfo(int id)
+        {
+            var urun = FGDB.DiscountTBL.Find(id);
+            
+            return View("CampaignUpdateinfo", urun);
+        }
+        [HttpGet]
+        public ActionResult ADDCampaign()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult ADDCampaign(DiscountTBL p1)
+        {
+            if (ModelState.IsValid)
+            {
+                FGDB.DiscountTBL.Add(p1);
+
+                FGDB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(p1);
+            }
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebApplication2.Models.Entity;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        FGDBEntities1 FGDB = new FGDBEntities1();
+        public ActionResult Index()
+        {
+            var context = FGDB.ProductTBL.ToList();
+            var discountContext = FGDB.DiscountTBL.ToList();
+            List<ProductTBL> updatedProducts = new List<ProductTBL>();
+
+            foreach (var product in context)
+            {
+                foreach (var discount in discountContext)
+                {
+
+                    DateTime currentDate = DateTime.Now;
+                    if (product.product_category == discount.discountedproducts && currentDate > discount.startdate && currentDate < discount.enddate)
+                    {
+                        decimal newPrice = (decimal)(product.product_price - (product.product_price * (discount.discountamount / Convert.ToDecimal(100.00))));
+                        product.product_price = newPrice;
+                        updatedProducts.Add(product);
+                    }
+                }
+            }
+
+
+            return View(context);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult CampaignUpdateinfo(int id)
+        {
+            var urun = FGDB.DiscountTBL.Find(id);
+
+            return View("CampaignUpdateinfo", urun);
+        }
+        [HttpGet]
+        public ActionResult ADDProduct()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult ADDProduct(ProductTBL p1)
+        {
+            if (ModelState.IsValid)
+            {
+                FGDB.ProductTBL.Add(p1);
+
+                FGDB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(p1);
+            }
+        }
+
+
+        public ActionResult RemoveProduct(int id)
+        {
+            var product = FGDB.ProductTBL.Find(id);
+            FGDB.ProductTBL.Remove(product);
+            FGDB.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+
+
+}
